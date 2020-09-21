20b9ed6fbe7d93807976ebf80347ead2837aef93
Christian Sourisseau <christian.sourisseau@etudiant.univ-rennes1.fr>
2020-09-21 16:53:57 +0200

    vue implemented, model start

--- interaction.js
+++ interaction.js
@@ -2,11 +2,47 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  // Définir ici les attributs de la 'classe'
+  this.posx_init = 0;
+  this.posy_init = 0;
+  this.posx_fin = 0;
+  this.posy_fin = 0;
+  this.mousedown = false;
 
 	// Developper les 3 fonctions gérant les événements
+  DnD.prototype.mouseDown = function(evt) {
+    //mouse down and take original position
+    this.mousedown = true;
+    this.posx_init = getMousePosition(canvas, evt).x;
+    this.posy_init = getMousePosition(canvas, evt).y;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+    //log events
+    console.log(this.posx_init);
+    console.log(this.posy_init);
+
+  }
+
+  DnD.prototype.mouseUp = function(evt) {
+    this.mousedown = false;
+    this.posx_fin = getMousePosition(canvas, evt).x;
+    this.posy_fin = getMousePosition(canvas, evt).y;
+
+      //log events
+      console.log(this.posx_fin);
+      console.log(this.posy_fin);
+  }
+
+  DnD.prototype.mouseMove = function(evt) {
+    if (this.mousedown) {
+       //draw something
+      console.log('moving');
+    }
+  }
+  
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.mouseDown, false);
+  canvas.addEventListener('mousemove', this.mouseMove, false);
+  canvas.addEventListener('mouseup', this.mouseUp, false);
 };
 
 
@@ -18,6 +54,5 @@ function getMousePosition(canvas, evt) {
     y: evt.clientY - rect.top
   };
 };
-
 
 
--- model.js
+++ model.js
@@ -1,3 +1,21 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function  Drawing() {
+    this.formarray() = new Array();
+}
+
+function Form() {
+    //color and line thickness
+}
+
+function Rectangle() {
+    // top left corner, width, heigth
+    //herite de form
+}
+
+function Line() {
+    //coordonates of two points
+    //herite de form
+
+}
