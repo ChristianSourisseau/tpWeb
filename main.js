3aebc92afe3e38d4dfdd1678ed13a948fdea3d4d
Christian Sourisseau <christian.sourisseau@etudiant.univ-rennes1.fr>
2020-09-22 13:08:39 +0200

    model vue implement with getters, needs debug

--- main.js
+++ main.js
@@ -7,20 +7,19 @@ canvas.height=600
 
 // Code temporaire pour tester le DnD
 new DnD(canvas);
-ctx.fillStyle = '#F0F0F0'; // set canvas' background color
-ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
-/////
+var drawing = new Drawing();
 
 // Code temporaire pour tester l'affiche de la vue
-//var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-//rec.paint(ctx);
-//var ligne = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
-//ligne.paint(ctx);
-// tester également Dessin.
-////
+var rec = new Rectangle(10, 20, 50, 100, 5, '#00CCC0');
+var ligne = new Line(10, 20, 50, 100, 5, '#00CCC0');
+
+drawing.addForm(rec);
+drawing.addForm(ligne);
+
+drawing.paint(ctx);
 
 // Code final à utiliser pour manipuler Pencil.
-//var drawing = new Drawing();
+
 //var pencil = new Pencil(ctx, drawing, canvas);
 //drawing.paint(ctx, canvas);
 
--- model.js
+++ model.js
@@ -1,21 +1,70 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
-function  Drawing() {
-    this.formarray() = new Array();
+function Drawing() {
+    this.formarray = new Array();
+
+    Drawing.prototype.getForms = function() {
+        return this.formarray;
+    }
+
+    Drawing.prototype.addForm = function(form) {
+        this.formarray.push(form)
+    }
 }
 
-function Form() {
-    //color and line thickness
+function Form(thickness, color) {
+    this.thickness = thickness;
+    this.color = color;
+
+    Form.prototype.getThickness = function() {
+        return this.thickness;
+    }
+    Form.prototype.getColor = function() {
+        return this.color;
+    }
 }
 
-function Rectangle() {
-    // top left corner, width, heigth
-    //herite de form
+function Rectangle(xinit, yinit, w, h, th, c) {
+    // top left corner
+    this.tlx = xinit;
+    this.tly = yinit;
+    this.brx = Math.abs(xinit - w);
+    this.bry = Math.abs(yinit - h);
+    Form.call(this, th, c);
+
+    Rectangle.prototype.getInitX = function() {
+        return this.tlx;
+    }
+    Rectangle.prototype.getInitY = function() {
+        return this.tly;
+    }
+    Rectangle.prototype.getFinX = function() {
+        return this.brx;
+    }
+    Rectangle.prototype.getFinY = function() {
+        return this.bry;
+    }
 }
 
-function Line() {
+function Line(initx, inity, finx, finy, th, c) {
     //coordonates of two points
-    //herite de form
+    this.posx_init = initx;
+    this.posy_init = inity;
+    this.posx_fin = finx;
+    this.posy_fin = finy;
+    Form.call(this, th, c);
 
+    Line.prototype.getInitX = function() {
+        return this.posx_init;
+    }
+    Line.prototype.getInitY = function() {
+        return this.posy_init;
+    }
+    Line.prototype.getFinX = function() {
+        return this.posx_fin;
+    }
+    Line.prototype.getFinY = function() {
+        return this.posy_fin;
+    }
 }
